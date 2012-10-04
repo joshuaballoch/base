6412efb21e225a622b914b3464411c7246427b9a
Joshua Balloch <joshuaballoch@gmail.com>
2012-10-04 16:36:41 +0800

    three picture layout

--- source/javascripts/base.js
+++ source/javascripts/base.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   // Scroll Distance for Sticky Header
-  var distance = $('.mid_container').offset().top,
-    $window = $(window);
-
+  var $window = $(window);
   $window.scroll(function() {
-    if ( $window.scrollTop() >= distance ) {
+    if ( $window.scrollTop() >= $('.upper_container').height() ) {
         // Your div has reached the top
         $(".mid_container").addClass("sticky");
         $(".lower_container").addClass("sticky2");
     }
   });
   $window.scroll(function() {
-    if ( $window.scrollTop() <= distance ) {
+    if ( $window.scrollTop() <= $('.upper_container').height() ) {
         // Your div has reached the top
         $(".mid_container").removeClass("sticky");
         $(".lower_container").removeClass("sticky2");
     }
+  });
+
+  //basic function for all pictures
+  $(".figure").hover(function(){
+    var w = $(this).children('.picture').children('img').width();
+    $(this).children('.picture').width(w);
+    $(this).children('.picture').children('.overlay').toggle();
+    $(this).children('.picture').children('.btn-group').toggle();
   });
 
 });
